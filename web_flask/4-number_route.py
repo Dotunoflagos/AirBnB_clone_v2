bf8ddf5acb280e9fd549fa1c65f25c4485d24bd2
dotunoflagos <adedotun_balogun@yahoo.com>
2023-02-24 14:13:58 +0100

    3

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Flask App
+"""
+from flask import Flask, request
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+port = 5000
+host = '0.0.0.0'
+
+
+@app.route('/')
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python(text):
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+@app.route('/number/int:<n>')
+def python(n):
+    if isinstance(n, int):
+        return '{} is a number'.format(n)
+
+
+if __name__ == "__main__":
+    """
+    Flask App
+    """
+    app.run(host=host, port=port)
