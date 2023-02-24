1ce774ae51df3dc680b89367a2cd9d7ec7279d31
dotunoflagos <adedotun_balogun@yahoo.com>
2023-02-24 14:24:22 +0100

    3

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Flask App
+"""
+from flask import Flask, request
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+port = 5000
+host = '0.0.0.0'
+
+
+@app.route('/')
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python(text):
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>')
+def intys(n):
+    if isinstance(n, int):
+        return '{} is a number'.format(n)
+
+@app.route('/number_template/<int:n>')
+def displ(n):
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    """
+    Flask App
+    """
+    app.run(host=host, port=port)
